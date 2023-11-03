a24e84d10f737259ac4a15fa0a1447141b0331dc
Marlon Oliveira <marlon.oliveira@tray.net.br>
2023-11-03 15:54:19 -0300

    melhorias visuais

--- client/src/components/Posts.vue
+++ client/src/components/Posts.vue
@@ -43,17 +43,35 @@ export default defineComponent({
 
 <template>
   New Post:
-  <div>
-    Title: <input type="text" v-model="title"><br />
-    Body: <textarea v-model="body" /><br />
+  <div class="form-post">
+    <div>
+      <label for="input-title">Title:</label>
+      <input type="text" id="input-title" v-model="title">
+    </div>
+    <div>
+      <label for="input-body">Body:</label>
+      <textarea id="input-body" v-model="body" />
+    </div>
     <button @click="createPost">Create Post</button>
   </div>
   <h1>Posts</h1>
 
   <div v-for="post in posts">
-    <div>
+    <div class="item-post">
       <h3>{{ post.title }}</h3>
       <div>{{ post.body }}</div>
     </div>
   </div>
 </template>
+
+<style type="text/css">
+.form-post label {
+  display: block;
+}
+.item-post {
+  background-color: #303030;
+  border-radius: 15px;
+  padding: 15px;
+  margin-bottom: 20px;
+}
+</style>
